46d90a27762a7fe043becb4e27752d485bdeb941
rafazzaky <rafiazzakymc@gmail.com>
2022-11-23 12:01:44 +0700

    initial commit

---
+++ src/routes.js
@@ -0,0 +1,33 @@
+const handler = require('./handler');
+// const {addNoteHandler} = require(',/handler');
+
+const routes = [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: handler.addNoteHandler, // addNoteHandler
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: handler.getAllNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: handler.getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: handler.editNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: handler.deleteNoteByIdHandler,
+  },
+];
+
+
+module.exports = routes;
